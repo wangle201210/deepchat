9506622318e4368c0b4ecfac551528f01a0f067b
zerob13 <zerob13@gmail.com>
2025-04-09 14:05:16 +0800

    add macos tray icon

--- src/main/presenter/trayPresenter.ts
+++ src/main/presenter/trayPresenter.ts
@@ -1,26 +1,29 @@
-import { Tray, Menu, app } from 'electron'
+import { Tray, Menu, app, nativeImage, NativeImage } from 'electron'
 import path from 'path'
 import { WindowPresenter } from './windowPresenter'
 
 export class TrayPresenter {
   private tray: Tray | null = null
   private windowPresenter: WindowPresenter
+  private iconPath: string
 
   constructor(windowPresenter: WindowPresenter) {
     this.windowPresenter = windowPresenter
+    this.iconPath = path.join(app.getAppPath(), 'resources')
     this.createTray()
   }
 
   private createTray() {
     // 根据平台选择不同的图标
-    const basePath = path.join(app.getAppPath(), 'resources')
-    const iconPath = path.join(
-      basePath,
-      process.platform === 'win32' ? 'win-tray.ico' : 'mac-tray.png'
-    )
-    console.log('iconPath', iconPath)
-
-    this.tray = new Tray(iconPath)
+    let image: NativeImage | undefined = undefined
+    if (process.platform === 'darwin') {
+      image = nativeImage.createFromPath(path.join(this.iconPath, 'macTrayTemplate.png'))
+      image = image.resize({ width: 24, height: 24 })
+      image.setTemplateImage(true)
+    } else {
+      image = nativeImage.createFromPath(path.join(this.iconPath, 'win_tray.ico'))
+    }
+    this.tray = new Tray(image)
     this.tray.setToolTip('DeepChat')
 
     const contextMenu = Menu.buildFromTemplate([
