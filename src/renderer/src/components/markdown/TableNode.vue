daea40fbfc163fc17807b5421f9dd0461a27a303
deepinsect <deepinsect@github.com>
2025-04-22 18:00:55 +0800

    config style of table

--- src/renderer/src/components/markdown/TableNode.vue
+++ src/renderer/src/components/markdown/TableNode.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="table-wrapper">
-    <table class="markdown-table">
+  <div class="overflow-x-auto max-w-full mb-4 shadow-sm border border-border rounded-lg">
+    <table class="border-collapse my-0 min-w-full table-fixed w-auto">
       <thead>
-        <tr>
-          <th v-for="(cell, index) in node.header.cells" :key="'header-' + index">
+        <tr class="border-b">
+          <th
+            v-for="(cell, index) in node.header.cells"
+            :key="'header-' + index"
+            class="p-2 text-left font-semibold bg-gray-100 dark:bg-zinc-900 dark:border-zinc-800 whitespace-nowrap"
+            :class="{
+              'border-r ': index < node.header.cells.length - 1
+            }"
+          >
             <NodeRenderer
               :nodes="cell.children"
               :message-id="messageId"
@@ -14,8 +21,22 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(row, rowIndex) in node.rows" :key="'row-' + rowIndex">
-          <td v-for="(cell, cellIndex) in row.cells" :key="'cell-' + rowIndex + '-' + cellIndex">
+        <tr
+          v-for="(row, rowIndex) in node.rows"
+          :key="'row-' + rowIndex"
+          :class="[
+            rowIndex % 2 === 1 ? 'bg-gray-100 dark:bg-zinc-900 dark:border-zinc-800' : '',
+            rowIndex < node.rows.length - 1 ? 'border-b dark:border-zinc-800' : ''
+          ]"
+        >
+          <td
+            v-for="(cell, cellIndex) in row.cells"
+            :key="'cell-' + rowIndex + '-' + cellIndex"
+            class="p-2 text-left whitespace-nowrap"
+            :class="{
+              'border-r ': cellIndex < row.cells.length - 1
+            }"
+          >
             <NodeRenderer
               :nodes="cell.children"
               :message-id="messageId"
@@ -65,32 +86,3 @@ defineProps<{
 // 定义事件
 defineEmits(['copy'])
 </script>
-
-<style scoped>
-.table-wrapper {
-  overflow-x: auto;
-  margin-bottom: 1rem;
-}
-
-.markdown-table {
-  border-collapse: collapse;
-  width: 100%;
-  margin: 0;
-}
-
-.markdown-table th,
-.markdown-table td {
-  padding: 0.5rem;
-  border: 1px solid #dfe2e5;
-  text-align: left;
-}
-
-.markdown-table th {
-  font-weight: 600;
-  background-color: #f6f8fa;
-}
-
-.markdown-table tr:nth-child(2n) {
-  background-color: #f6f8fa;
-}
-</style>
