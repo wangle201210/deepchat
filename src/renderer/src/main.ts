d2740040b621b929703adb0ec99d62b7e98210a3
happer <happer9527@gmail.com>
2025-04-24 00:15:48 +0800

    finish rework of markdown

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -8,6 +8,7 @@ import App from './App.vue'
 import router from './router'
 import { createI18n } from 'vue-i18n'
 import locales from './i18n'
+import 'katex/dist/katex.min.css'
 
 const i18n = createI18n({
   locale: 'zh-CN',
